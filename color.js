d74be55b09c29c5b7e66b63a44bff1489ec44b36
cwsctstest002 <82876581+cwsctstest002@users.noreply.github.com>
2025-12-12 15:16:57 +0530

    Update color.js

--- color.js
+++ color.js
@@ -1,11 +1,15 @@
+// function getRemoteColor() {
+//     // You can put ANY logic here
+//     // You can compute, fetch, randomize, or apply rules
+
+//     const colors = ["#FF5733", "#4CAF50", "#2196F3", "#9C27B0"];
+
+//     // Example rule: pick based on time
+//     const index = new Date().getSeconds() % colors.length;
+
+//     return colors[index];
+// }
+
 function getRemoteColor() {
-    // You can put ANY logic here
-    // You can compute, fetch, randomize, or apply rules
-
-    const colors = ["#FF5733", "#4CAF50", "#2196F3", "#9C27B0"];
-
-    // Example rule: pick based on time
-    const index = new Date().getSeconds() % colors.length;
-
-    return colors[index];
+    return "#ff5733";
 }
